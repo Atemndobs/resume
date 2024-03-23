414efe6ed88f35ba8dac071cab1f38c2b1097102
Bertrand Atemkeng <atemnddobs@yahoo.com>
2024-03-23 21:55:32 +0100

    update and add link sections

--- src/components/Sections/Resume/index.tsx
+++ src/components/Sections/Resume/index.tsx
@@ -1,10 +1,38 @@
-import {FC, memo} from 'react';
+import { FC, memo } from "react";
 
-import {education, experience, SectionId, skills} from '../../../data/data';
-import Section from '../../Layout/Section';
-import ResumeSection from './ResumeSection';
-import {SkillGroup} from './Skills';
-import TimelineItem from './TimelineItem';
+import {
+  DevicePhoneMobileIcon,
+  EnvelopeIcon,
+  MapPinIcon,
+} from "@heroicons/react/24/outline";
+import classNames from "classnames";
+
+import { education, experience, SectionId, skills } from "../../../data/data";
+import Section from "../../Layout/Section";
+import ResumeSection from "./ResumeSection";
+import { SkillGroup } from "./Skills";
+import TimelineItem from "./TimelineItem";
+// Assuming ContactValueMap is exported from the same file as your contact data
+import { contact } from '../../../data/links';
+
+const ContactValueMap: Record<ContactType, ContactValue> = {
+  [ContactType.Email]: { Icon: EnvelopeIcon, srLabel: "Email" },
+  [ContactType.Phone]: { Icon: DevicePhoneMobileIcon, srLabel: "Phone" },
+  [ContactType.Location]: { Icon: MapPinIcon, srLabel: "Location" },
+  [ContactType.Github]: { Icon: GithubIcon, srLabel: "Github" },
+  [ContactType.LinkedIn]: { Icon: LinkedInIcon, srLabel: "LinkedIn" },
+  [ContactType.Facebook]: { Icon: FacebookIcon, srLabel: "Facebook" },
+  [ContactType.Twitter]: { Icon: TwitterIcon, srLabel: "Twitter" },
+  [ContactType.Instagram]: { Icon: InstagramIcon, srLabel: "Instagram" },
+};
+
+import Socials from "../../Socials";
+import {ContactType, ContactValue} from "../../../data/dataDef";
+import GithubIcon from "../../Icon/GithubIcon";
+import LinkedInIcon from "../../Icon/LinkedInIcon";
+import FacebookIcon from "../../Icon/FacebookIcon";
+import TwitterIcon from "../../Icon/TwitterIcon";
+import InstagramIcon from "../../Icon/InstagramIcon";
 
 const Resume: FC = memo(() => {
   return (
@@ -21,17 +49,51 @@ const Resume: FC = memo(() => {
           ))}
         </ResumeSection>
         <ResumeSection title="Skills">
-          <p className="pb-8">Here you can show a snapshot of your skills to show off to employers</p>
+          <p className="pb-8">
+            Home selected skills that I have acquired over the years.
+          </p>
           <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
             {skills.map((skillgroup, index) => (
-              <SkillGroup key={`${skillgroup.name}-${index}`} skillGroup={skillgroup} />
+              <SkillGroup
+                key={`${skillgroup.name}-${index}`}
+                skillGroup={skillgroup}
+              />
             ))}
           </div>
+        </ResumeSection>
+        <ResumeSection title="Links">
+          <p className="pb-8">
+            You can find me on the following platforms.
+          </p>
+          <div className="grid grid-cols-1 gap-6">
+            {contact.items.map(({type, text, href}) => {
+              const {Icon, srLabel} = ContactValueMap[type];
+              return (
+                  <a
+                      key={srLabel}
+                      href={href}
+                      target="_blank"
+                      className="flex items-center space-x-3  hover:text-orange-500"
+                  >
+                    <Icon aria-hidden="true" className="h-5 w-5"/>
+                    <span>{text}</span>
+                  </a>
+              );
+            })}
+          </div>
+
         </ResumeSection>
       </div>
     </Section>
   );
 });
 
-Resume.displayName = 'Resume';
+Resume.displayName = "Resume";
 export default Resume;
+
+
+// {
+//   type: ContactType.Github,
+//       text: "Atemndobs",
+//     href: "https://github.com/Atemndobs",
+// },
